5e029d923baa11212356f1656406eb09d2ed13ab
Amit Singh <amitsinghrajawat25@gmail.com>
2024-11-21 17:24:13 +0530

    update social links

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -13,13 +13,13 @@ function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <FaAws />
